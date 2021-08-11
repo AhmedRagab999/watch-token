e4eb286f9028ed5091b2e3b045fbec3e131ccf5b
erc20bep20 <84326904+erc20bep20@users.noreply.github.com>
2021-08-11 18:19:54 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -15,15 +15,15 @@ module.exports = {
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' }],
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/watch-token' }],
-    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'og:url', content: 'https://erc20bep20.github.io/watch-token' }],
+    ['meta', { property: 'og:image', content: 'https://erc20bep20.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:image', content: 'https://erc20bep20.github.io/watch-token/assets/images/watch-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'WatchToken | Create a Widget for your ERC20 or BEP20 Token' }],
     ['script', { src: '/assets/js/web3.min.js' }],
     ['script',
       {
-        src: 'https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=en&theme=momh&thirdparty=1&always=1&noGeoIp=1&scrolling=1&hideDetailsBtn=1', // eslint-disable-line max-len
+        src: '', // eslint-disable-line max-len
         defer: true,
       },
     ],
